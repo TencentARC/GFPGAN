4f0562df64fc780a5faec1e9feeb6d2636a18ffd
Xintao <wxt1994@126.com>
2021-10-22 16:11:17 +0800

    fix setup bug

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def get_hash():
         sha = get_git_hash()[:7]
     elif os.path.exists(version_file):
         try:
-            from facexlib.version import __version__
+            from gfpgan.version import __version__
             sha = __version__.split('+')[-1]
         except ImportError:
             raise ImportError('Unable to get git version')
