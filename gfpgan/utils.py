c48535f6141c21119aa1f879cdc82216bb83a47b
Nguyen Xuan Hung <hung59349214@gmail.com>
2023-11-14 19:43:29 +0700

    Update utils.py

--- gfpgan/utils.py
+++ gfpgan/utils.py
@@ -88,7 +88,7 @@ class GFPGANer():
 
         if model_path.startswith('https://'):
             model_path = load_file_from_url(
-                url=model_path, model_dir=os.path.join(ROOT_DIR, '~/gfpgan/weights'), progress=True, file_name=None)
+                url=model_path, model_dir='~/gfpgan/weights', progress=True, file_name=None)
         loadnet = torch.load(model_path)
         if 'params_ema' in loadnet:
             keyname = 'params_ema'
